dc8d64afc8f85f4eb40b0dff10d96e4b136ec1cb
Jason M. Dudley <j.m.duds@gmail.com>
2026-01-21 20:35:29 -0600

    Make build table name more unique

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -5,7 +5,7 @@ public class Configs {
     public static final String DEPLOY_METADATA_PATH = "/home/lvuser/537metadata.txt";
 
     // NetworkTables locations (Elastic can display String topics easily)
-    public static final String NT_TABLE = "Build";
+    public static final String NT_TABLE = "Build537";
     public static final String NT_RAW_KEY = "DeployMetadataRaw";   // full file contents
     public static final String NT_STATUS_KEY = "DeployMetadataStatus"; // diag/status text
     public static final String NT_PREFIX_PARSED = "DeployMetadata/";   // per-field entries

29df86aae11ebf779f7f912c553bdb1e36d55aa8
David <davidpolcen@gmail.com>
2026-01-31 11:36:27 -0600

    updated transfer to Neo

--- src/main/java/frc/robot/subsystems/TransferSubsystem.java
+++ src/main/java/frc/robot/subsystems/TransferSubsystem.java
@@ -1,7 +1,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -12,17 +15,36 @@ import frc.robot.Constants;
 
 public class TransferSubsystem extends SubsystemBase {
     public TalonFX transfer;
+    private SparkMax transferNeo;
+    private SparkMaxConfig transferConfig;
 
     //Configuration for the transfer subsystem
     public TransferSubsystem() {
-        transfer = new TalonFX(Constants.Transfer.TRANSFER_MOTOR_ID);
-        transfer.getConfigurator().apply(Configs.TRANSFER_CONFIG);
+        //transfer = new TalonFX(Constants.Transfer.TRANSFER_MOTOR_ID);
+        //transfer.getConfigurator().apply(Configs.TRANSFER_CONFIG);
+
+        transferConfig = new SparkMaxConfig();
+
+        transferConfig
+            .idleMode(IdleMode.kBrake)
+            .smartCurrentLimit(Constants.Transfer.CURRENT_LIMIT)
+            .inverted(false);
+        transferConfig.encoder
+            .positionConversionFactor(Constants.Transfer.ENCODER_FACTOR)
+            .velocityConversionFactor(Constants.Transfer.ENCODER_FACTOR / 60.0);
+        transferConfig.closedLoop
+            .pid(
+                Constants.Transfer.KP,
+                Constants.Transfer.KI,
+                Constants.Transfer.KD
+            );
     }
-
     public void setVelocity(double velocity) {
-        VelocityVoltage velocityRequest = new VelocityVoltage(velocity);
+        //VelocityVoltage velocityRequest = new VelocityVoltage(velocity);
 
-        transfer.setControl(velocityRequest);
+       // transfer.setControl(velocityRequest);
+
+       transferNeo.getClosedLoopController().setSetpoint(1.0, ControlType.kVelocity);
     }
 
     public Command getSetVelocityCommand(double velocity) {
